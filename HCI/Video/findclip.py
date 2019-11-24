cfbc757bb3fdf7fec02a4d6c9381dca30c7efaad
Saong <771256240@qq.com>
2019-11-24 11:20:23 +0800

    12

--- HCI/Video/findclip.py
+++ HCI/Video/findclip.py
@@ -9,8 +9,8 @@ def findclip(request):
     print(request.POST["query"])
     f2 = request.FILES.get('video')
     # 文件保存路径
-    #fname ='/data02/chengjian19/videos/'+f2.name
-    fname = '.\\uploadvideo\\' + f2.name
+    fname ='/data02/chengjian19/videos/'+f2.name
+    #fname = '.\\uploadvideo\\' + f2.name
     with open(fname, 'wb') as pic:
         for c in f2.chunks():
             pic.write(c)
