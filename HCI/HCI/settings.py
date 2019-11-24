9cccffc256e65b5489b7f7a499cd2ff689dee97a
Saong <771256240@qq.com>
2019-11-24 10:35:43 +0800

    冲

--- HCI/HCI/settings.py
+++ HCI/HCI/settings.py
@@ -119,3 +119,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+#STATIC_ROOT = '/data02/chengjian19/HCI/sjc/HCIWeb/HCI/static'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+#STATIC_ROOT = os.path.join(BASE_DIR, "/staticnew")
