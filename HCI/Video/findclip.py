0614affb8ff60368d9f72175714a66c48e7b3fbf
Saong <771256240@qq.com>
2019-12-19 10:27:48 +0800

    修改提示

--- HCI/Video/findclip.py
+++ HCI/Video/findclip.py
@@ -9,21 +9,25 @@ def findclip(request):
     print(request.POST["query"])
     f2 = request.FILES.get('video')
     # 文件保存路径
-    """
     fname ='/data02/chengjian19/videos/'+f2.name
     #fname = '.\\uploadvideo\\' + f2.name
     with open(fname, 'wb') as pic:
         for c in f2.chunks():
             pic.write(c)
-    """
     url = 'http://12.12.12.3:7009/'
     data = {}
-    data['sentence'] = 'The person takes out a cutting board from the drawer'
-    data['video_name'] = 's27-d70.avi'
-    #data['sentence'] = request.POST["query"]
-    #data['video_name'] = f2.name
-    #r = requests.post(url,data)
+    #data['sentence'] = 'The person takes out a cutting board from the drawer'
+    #data['video_name'] = 's27-d70.avi'
+    data['sentence'] = request.POST["query"]
+    data['video_name'] = f2.name
+    r = requests.post(url,data)
     context = {}
-    context['answer'] = '1111'
-    print(context['answer'])
+    context['answer'] = '1233'
+    print(r.content)
+    content = json.loads(r.content)
+    print(content)
+    context['answer'] = content
+    #a = r.content['end1']
+    #context['answer'] = r.json()[0]['end1']
+    #print(context['answer'])
     return HttpResponse(json.dumps(context),content_type='application/json')
